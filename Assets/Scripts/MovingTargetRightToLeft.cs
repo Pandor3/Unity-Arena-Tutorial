bd3927a1579121f545854401d261e455a0c18090
Pandor3 <ThatDumbPhilosopher@gmail.com>
2025-02-11 11:39:13 +0100

    Creation et edition d'un nouveau script : MovingTargetRightToLeft

---
+++ Assets/Scripts/MovingTargetRightToLeft.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class MovingTarget : MonoBehaviour
+{
+    public float speed = 2f;
+    public float moveDistance = 5f;
+
+    private Vector3 startPosition;
+    private bool movingRight = true;
+
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        if (movingRight)
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+            if (transform.position.x >= startPosition.x + moveDistance)
+            {
+                movingRight = false;
+            }
+        }
+        else 
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+            if (transform.position.x <= startPosition.x - moveDistance)
+            {
+                movingRight = true;
+            }
+        }
+    }
+}
